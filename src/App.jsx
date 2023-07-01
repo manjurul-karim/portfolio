5987df501f4f7c5f805785d8af928388d5011d9b
manjurul17002 <manjurul17002@gmail.com>
2023-07-01 22:03:16 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import SocilaLinks from "./components/SocilaLinks";
+
 function App() {
-  return <>
-  <h2 className="bg-blue-600 text-4xl text-center">hello</h2>
-  </>;
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <SocilaLinks />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import arrayDestruct from "../assets/projects/arrayDestruct.jpg";
+import installNode from "../assets/projects/installNode.jpg";
+import navbar from "../assets/projects/navbar.jpg";
+import reactParallax from "../assets/projects/reactParallax.jpg";
+import reactSmooth from "../assets/projects/reactSmooth.jpg";
+import reactWeather from "../assets/projects/reactWeather.jpg";
+
+
+const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      src: arrayDestruct,
+    },
+    {
+      id: 2,
+      src: reactParallax,
+    },
+    {
+      id: 3,
+      src: navbar,
+    },
+    {
+      id: 4,
+      src: reactSmooth,
+    },
+    {
+      id: 5,
+      src: installNode,
+    },
+    {
+      id: 6,
+      src: reactWeather,
+    },
+  ];
+
+  return (
+    <div
+      name="projects"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="md:pt-24">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Projects
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {projects.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
